99d8a9f7817a483464cbd189e60ae68e4e084e49
laroself1 <laroself1@gmail.com>
2016-02-20 00:22:20 +0200

    Hello

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+ //   List<Integer> arr = new ArrayList<Integer>();
+    int i,p;
+        int arr [][]= {{1,5,9,13},{2,6,10,14},{3,7,11,15},{4,8,12,16}};
+        for (i=0; i<arr.length;i++){
+            for (p=0; p<arr.length; p++){
+                ;System.out.print(arr[i][p] +" ");
+            }
+            System.out.println();}}}
